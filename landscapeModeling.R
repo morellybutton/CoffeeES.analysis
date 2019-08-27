4e919d1e221e9fa1dd3749367d6e52f204ed61b3
morellybutton <alexandra.morel@gmail.com>
2019-08-27 16:30:11 +0100

    First stab at modeling the landscape, using model averaging.

--- landscapeModeling.R
+++ landscapeModeling.R
@@ -1,8 +1,8 @@
 #code to model distribution of yields under different landscape configurations
 
 library(tidyverse)
-library(AICcmodavg)
 library(arm)
+library(MuMIn)
 
 setwd("/Volumes/ELDS/ECOLIMITS/Ethiopia/Yayu")
 
@@ -23,7 +23,7 @@ cand.set.14[[3]]<-lm(Shrub.kg~rescale(Shannon.i)*rescale(BA.legume) +
 cand.set.14[[4]]<-lm(Shrub.kg~rescale(Shannon.i)+rescale(BA.legume) +
                        rescale(elevation) + rescale(tmax.anom.fruit),data=d.F.new.14)
 
-topmodels.avg<-MuMIn::model.avg(cand.set.14) 
+topmodels.avg<-model.avg(cand.set.14) 
 
 tmp.14<-read.csv(paste0(getwd(),"/Analysis/ES/Model.Average_yld14.delta6.v3.confint.csv"))
 
@@ -48,7 +48,7 @@ cand.set.15[[13]]<-lm(logdiff~rescale(BA.legume)  + rescale(patcharea),data=d.F.
 cand.set.15[[14]]<-lm(logdiff~rescale(BA.legume) + rescale(propCBD) + rescale(coffee.area.ha)+
                         rescale(patcharea) + rescale(Shannon.i),data=d.F.new15)
 
-topmodels.avg.15<-MuMIn::model.avg(cand.set.15) 
+topmodels.avg.15<-model.avg(cand.set.15) 
 tmp.15<-read.csv(paste0(getwd(),"/Analysis/ES/Model.Average_logylddiff15.delta2.v3.confint.csv"))
 
 #for 2016 yield difference
@@ -58,7 +58,7 @@ cand.set.16[[2]]<-lm(logdiff~rescale(Shannon.i)*rescale(BA.legume) + rescale(ele
 cand.set.16[[3]]<-lm(logdiff~rescale(Shannon.i)*rescale(BA.legume) + rescale(elevation) +rescale(tmax.anom.fruit) + rescale(propCBB),data=d.F.new16)
 cand.set.16[[4]]<-lm(logdiff~rescale(Shannon.i)+rescale(BA.legume) + rescale(elevation) +rescale(tmax.anom.fruit),data=d.F.new16)
 
-topmodels.avg.16<-MuMIn::model.avg(cand.set.16)
+topmodels.avg.16<-model.avg(cand.set.16)
 tmp.16<-read.csv(paste0(getwd(),"/Analysis/ES/Model.Average_logylddiff16.delta2.v3.confint.csv"))
 
 #keep elevation the same for each farmer (because that is outside their control)
@@ -69,18 +69,251 @@ tmp.16<-read.csv(paste0(getwd(),"/Analysis/ES/Model.Average_logylddiff16.delta2.
 #produce figures for normal year, then log yield difference for "hot" year (2015), "dry" year (2016)
 
 #modelling "normal" year
-#add min and max patch area
-d.F.new.14$min.patcharea<-d.F.new.14 %>% dplyr::select(patcharea) %>% min()
-d.F.new.14$max.patcharea<-d.F.new.14 %>% dplyr::select(patcharea) %>% max()
+d.F.new.14$`rescale(patcharea)` <- rescale(d.F.new.14$patcharea)
+d.F.new.14$`rescale(elevation)` <- rescale(d.F.new.14$elevation)
+d.F.new.14$`rescale(GapDry)` <- rescale(d.F.new.14$GapDry)
+d.F.new.14$`rescale(tmax.anom.fruit)` <- rescale(d.F.new.14$tmax.anom.fruit)
 
+#add min and max patch area
+d.F.new.14$min.patcharea <-
+  d.F.new.14$`rescale(patcharea)`[match(d.F.new.14 %>% dplyr::select(patcharea) %>% min(), d.F.new.14$patcharea)]
+  
+d.F.new.14$max.patcharea<- 
+  d.F.new.14$`rescale(patcharea)`[match(d.F.new.14 %>% dplyr::select(patcharea) %>% max(), d.F.new.14$patcharea)]
+
+d.F.new.14$mean.patcharea<- 0
+  
 #add min and max BA legume
-d.F.new.14$min.BA.legume<-d.F.new.14 %>% dplyr::select(BA.legume) %>% min()
-d.F.new.14$max.BA.legume<-d.F.new.14 %>% dplyr::select(BA.legume) %>% max()
+d.F.new.14$`rescale(BA.legume)` <- rescale(d.F.new.14$BA.legume)
+
+d.F.new.14$min.BA.legume<-
+  d.F.new.14$`rescale(BA.legume)`[match(d.F.new.14 %>% dplyr::select(BA.legume) %>% min(), d.F.new.14$BA.legume)]
+
+d.F.new.14$max.BA.legume<-
+  d.F.new.14$`rescale(BA.legume)`[match(d.F.new.14 %>% dplyr::select(BA.legume) %>% max(), d.F.new.14$BA.legume)]
+
+d.F.new.14$mean.BA.legume<-0
 
 #add min and max shade diversity
-d.F.new.14$min.Shannon.i<-d.F.new.14 %>% dplyr::select(Shannon.i) %>% min()
-d.F.new.14$max.Shannon.i<-d.F.new.14 %>% dplyr::select(Shannon.i) %>% max()
+d.F.new.14$`rescale(Shannon.i)` <- rescale(d.F.new.14$Shannon.i)
 
-#create dataframe for min patch area and normal year
-df.pa.min.norm <- d.F.new.14 %>% dplyr::select(Plot,Shrub.kg,Shannon.i,BA.legume,elevation,min.patcharea,labour,CN.ratio) %>% 
-  rename(patcharea=min.patcharea)
+d.F.new.14$min.Shannon.i<-
+  d.F.new.14$`rescale(Shannon.i)`[match(d.F.new.14 %>% dplyr::select(Shannon.i) %>% min(), d.F.new.14$Shannon.i)]
+
+d.F.new.14$max.Shannon.i<-
+  d.F.new.14$`rescale(Shannon.i)`[match(d.F.new.14 %>% dplyr::select(Shannon.i) %>% max(), d.F.new.14$Shannon.i)]
+
+d.F.new.14$mean.Shannon.i<-0
+  
+#create dataframe for min patch area and all shade variables for normal year
+df.pa.min.norm <- d.F.new.14 %>% dplyr::select(Plot,Shrub.kg,`rescale(Shannon.i)`,`rescale(BA.legume)`,max.BA.legume,min.BA.legume,mean.BA.legume,min.Shannon.i,max.Shannon.i,mean.Shannon.i,
+                                               `rescale(elevation)`,min.patcharea,`rescale(tmax.anom.fruit)`,`rescale(GapDry)`) %>% 
+  rename(patcharea=min.patcharea) 
+
+#create dataframe for mean patch area and all shade variables for normal year
+df.pa.mean.norm <- d.F.new.14 %>% dplyr::select(Plot,Shrub.kg,`rescale(Shannon.i)`,`rescale(BA.legume)`,max.BA.legume,min.BA.legume,mean.BA.legume,min.Shannon.i,max.Shannon.i,mean.Shannon.i,
+                                                `rescale(elevation)`,mean.patcharea,`rescale(tmax.anom.fruit)`,`rescale(GapDry)`) %>% 
+  rename(patcharea=mean.patcharea) 
+
+#create dataframe for max patch area and all shade variables for normal year
+df.pa.max.norm <- d.F.new.14 %>% dplyr::select(Plot,Shrub.kg,`rescale(Shannon.i)`,`rescale(BA.legume)`,max.BA.legume,min.BA.legume,mean.BA.legume,min.Shannon.i,max.Shannon.i,mean.Shannon.i,
+                                               `rescale(elevation)`,max.patcharea,`rescale(tmax.anom.fruit)`,`rescale(GapDry)`) %>% 
+  rename(patcharea=max.patcharea)
+
+#max patcharea and variation in shade variables
+df.pa.max.norm.1<-df.pa.max.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=min.Shannon.i,BA.legume=max.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.max.norm.1<-df.pa.max.norm.1 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.max.norm.1<-left_join(df.pa.max.norm.1,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+#norm1<-ggplot(df.pa.max.norm.1,aes(elevation,Shrub.kg.mod)) + geom_bar(stat="identity",position = position_dodge(width = 0.9))
+norm1<-ggplot(df.pa.max.norm.1,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.max.norm.2<-df.pa.max.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=mean.Shannon.i,BA.legume=mean.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.max.norm.2<-df.pa.max.norm.2 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.max.norm.2<-left_join(df.pa.max.norm.2,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm2<-ggplot(df.pa.max.norm.2,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.max.norm.3<-df.pa.max.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=max.Shannon.i,BA.legume=min.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.max.norm.3<-df.pa.max.norm.3 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.max.norm.3<-left_join(df.pa.max.norm.3,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm3<-ggplot(df.pa.max.norm.3,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+
+#mean patcharea and variation in shade variables
+df.pa.mean.norm.1<-df.pa.mean.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=min.Shannon.i,BA.legume=max.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.mean.norm.1<-df.pa.mean.norm.1 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.mean.norm.1<-left_join(df.pa.mean.norm.1,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+#norm1<-ggplot(df.pa.max.norm.1,aes(elevation,Shrub.kg.mod)) + geom_bar(stat="identity",position = position_dodge(width = 0.9))
+norm4<-ggplot(df.pa.mean.norm.1,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.mean.norm.2<-df.pa.mean.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=mean.Shannon.i,BA.legume=mean.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.mean.norm.2<-df.pa.mean.norm.2 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.mean.norm.2<-left_join(df.pa.mean.norm.2,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm5<-ggplot(df.pa.mean.norm.2,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.mean.norm.3<-df.pa.mean.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=max.Shannon.i,BA.legume=min.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.mean.norm.3<-df.pa.mean.norm.3 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.mean.norm.3<-left_join(df.pa.mean.norm.3,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm6<-ggplot(df.pa.mean.norm.3,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+#min patcharea and variation in shade variables
+df.pa.min.norm.1<-df.pa.min.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=min.Shannon.i,BA.legume=max.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.min.norm.1<-df.pa.min.norm.1 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.min.norm.1<-left_join(df.pa.min.norm.1,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+#norm1<-ggplot(df.pa.max.norm.1,aes(elevation,Shrub.kg.mod)) + geom_bar(stat="identity",position = position_dodge(width = 0.9))
+norm7<-ggplot(df.pa.min.norm.1,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.min.norm.2<-df.pa.min.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=mean.Shannon.i,BA.legume=mean.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.min.norm.2<-df.pa.min.norm.2 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.min.norm.2<-left_join(df.pa.min.norm.2,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm8<-ggplot(df.pa.min.norm.2,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+df.pa.min.norm.3<-df.pa.min.norm %>% dplyr::select(-`rescale(Shannon.i)`,-`rescale(BA.legume)`) %>% 
+  rename(Shannon.i=max.Shannon.i,BA.legume=min.BA.legume,elevation=`rescale(elevation)`,tmax.anom.fruit=`rescale(tmax.anom.fruit)`,GapDry=`rescale(GapDry)`) %>% 
+  dplyr::select(BA.legume,elevation,GapDry,patcharea,Shannon.i,tmax.anom.fruit,Plot,Shrub.kg)
+
+
+df.pa.min.norm.3<-df.pa.min.norm.3 %>% group_by(Plot) %>% 
+  mutate(Shrub.kg.mod=tmp.14[tmp.14$Comparison=="(Intercept)","Estimate"]+tmp.14[tmp.14$Comparison=="rescale(elevation)","Estimate"]*elevation +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume)","Estimate"]*BA.legume +
+           tmp.14[tmp.14$Comparison=="rescale(Shannon.i)","Estimate"]*Shannon.i +
+           tmp.14[tmp.14$Comparison=="rescale(patcharea)","Estimate"]*patcharea +
+           tmp.14[tmp.14$Comparison=="rescale(GapDry)","Estimate"]*GapDry +
+           tmp.14[tmp.14$Comparison=="rescale(tmax.anom.fruit)","Estimate"]*tmax.anom.fruit +
+           tmp.14[tmp.14$Comparison=="rescale(BA.legume):rescale(Shannon.i)","subset"]*BA.legume*Shannon.i)
+
+df.pa.min.norm.3<-left_join(df.pa.min.norm.3,d.F.new.14 %>% dplyr::select(Plot,elevation), by="Plot") %>%
+  rename(elevation=elevation.y)
+
+norm9<-ggplot(df.pa.min.norm.3,aes(Shrub.kg.mod)) + geom_freqpoly(binwidth=0.05) + theme_classic() + xlab("Modelled Yield [kg/shrub]") +
+  theme(text=element_text(size=14)) + xlim(0,1.8)
+
+# Create a figure by combining the different plots
+figure <- ggpubr::ggarrange(norm1, norm2, norm3,norm4,norm5,norm6,norm7,norm8,norm9, labels = "AUTO")
+
+# Annotate the figure by adding a common labels
+ggpubr::annotate_figure(figure,
+                top = ggpubr::text_grob(expression('Min Shade Diversity' %->% 'Max Shade Diversity'), face = "bold", size = 16),
+                bottom = ggpubr::text_grob(expression('Max Basal Area of Leguminous Trees' %<-% 'Min Basal Area of Leguminous Trees'), face = "bold", size = 16),
+                left = ggpubr::text_grob(expression('Min Patch Area' %->% 'Max Patch Area'), rot = 90, face = "bold", size = 16),
+                fig.lab = "Normal Year", fig.lab.face = "bold", fig.lab.size = 20
+)
